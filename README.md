fac168554384a1b3c0b3c206a820cdc517d9c96b
Zatho <thom.reix4@outlook.com>
2025-12-15 18:38:04 +0100

    Update README, enhance CSS styles, and improve search functionality with pagination and button labels

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Projet-Cinema
+
+Lien du repot github : https://github.com/Zatho0/Projet-Cinema
+
--- js/search.js
+++ js/search.js
@@ -12,8 +12,8 @@ function moreMovie() {
   result();
 }
 function lessMovie() {
-  page = 1;
-  searchResult.innerHTML = "";
+  page -= 1;
+  
   result();
 }
 
@@ -25,18 +25,19 @@ function result() {
       "<p>Veuillez entrer au moins 3 caractères pour la recherche.</p>";
     loadMoreBtn.style.display = "none";
     loadLessBtn.style.display = "none";
+    page = 1;
     return;
   }
 
   fetchSearch(query, page)
     .then((data) => {
+        searchResult.innerHTML = "";
       if (data.Response === "True") {
         data.Search.forEach((movie) => {
           let movieDiv = document.createElement("div");
           movieDiv.innerHTML = `
                         <img src="${movie.Poster}" alt="${movie.Title} Poster">
                         <h3>${movie.Title}</h3>
-                        
                         <a href="movie.html?id=${movie.imdbID}">Détails</a>
                     `;
           searchResult.appendChild(movieDiv);
@@ -49,6 +50,7 @@ function result() {
         searchResult.innerHTML = "<p>Aucun résultat trouvé</p>";
         loadLessBtn.style.display = "none";
         loadMoreBtn.style.display = "none";
+        page = 1;
       }
     })
     .catch((err) => console.error("Erreur:", err));
@@ -56,11 +58,14 @@ function result() {
 searchInput.addEventListener("input", result);
 loadMoreBtn.addEventListener("click", () => {
   moreMovie();
-  loadMoreBtn.style.display = "none";
+
   loadLessBtn.style.display = "block";
 });
 loadLessBtn.addEventListener("click", () => {
   lessMovie();
   loadMoreBtn.style.display = "block";
-  loadLessBtn.style.display = "none";
+    if (page === 1) { 
+        loadLessBtn.style.display = "none";
+    }
+  
 });
